6c46ce0d62bb67cccea54520401e3afbbd725ea8
BSVKliford <kliford@lenta.ru>
2022-04-07 22:13:02 +0300

    Edit project

--- src/ru/netology/worker/Worker.java
+++ src/ru/netology/worker/Worker.java
@@ -8,23 +8,21 @@ public class Worker {
         this.callback = callback;
     }
 
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
     public Worker(OnTaskErrorListener errorCallback) {
         this.errorCallback = errorCallback;
     }
 
     public void start() {
-        OnTaskErrorListener errorListener = System.out::println;
-        Worker workerError = new Worker(errorListener);
         for (int i = 0; i < 100; i++) {
             if (i == 33) {
-                workerError.setErrorCallback(i);
-                continue;
+                errorCallback.onError("Task " + i + " is error");
             }
             callback.onDone("Task " + i + " is done");
         }
-    }
-
-    public void setErrorCallback(int i) {
-        errorCallback.onError("Task " + i + " is error");
     }
 }
